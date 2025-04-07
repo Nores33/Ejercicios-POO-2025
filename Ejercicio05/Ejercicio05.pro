b151bf1f9cdb825fee54d6c6a35a3927e49b3ca8
Tristán Nores <45902437+Nores33@users.noreply.github.com>
2025-04-07 20:00:23 -0300

    Se sube Ejercicio 5
    Ahora si finalizado

--- Ejercicio05/Ejercicio05.pro
+++ Ejercicio05/Ejercicio05.pro
@@ -6,5 +6,4 @@ QT = core gui
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 SOURCES += \
-    main.cpp \
     main.cpp
--- Ejercicio05/main.cpp
+++ Ejercicio05/main.cpp
@@ -2,17 +2,37 @@
 #include <QLabel>
 #include <QPixmap>
 #include <QTimer>
+#include <QScreen>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    QWidget * ventana = new QWidget;
-    ventana->setWindowTitle( "Imagen" );
-    ventana->resize( 800, 600 );
-
+    // Crear la etiqueta (QLabel)
     QLabel *label = new QLabel;
+    label->setWindowTitle("Imagen");
+    label->setAlignment(Qt::AlignCenter);
 
-    QPixmap imagen("../imagen.jpg");
+    // Cargar la imagen
+    QPixmap imagen("../../pcb.jpg");
+    if (imagen.isNull()) {
+        qWarning("No se pudo cargar la imagen");
+        return -1;
+    }
+
+    // Obtener tamaño de pantalla principal
+    QSize pantallaSize = QGuiApplication::primaryScreen()->availableSize();
+
+    // Escalar la imagen al tamaño de la pantalla, manteniendo proporciones
+    QPixmap escalada = imagen.scaled(pantallaSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+    // Pegar la imagen escalada en el QLabel
+    label->setPixmap(escalada);
+
+    // Mostrar en pantalla completa
+    label->showMaximized();
+
+    // Cerrar aplicación después de 3 segundos
+    QTimer::singleShot(3000, &app, &QApplication::quit);
 
     return app.exec();
 }
